40a824160e3d13cff8aba9eccc2282fcc236b95e
githug-o <146291968+githug-o@users.noreply.github.com>
2023-09-28 09:02:12 -0300

    Update calcTED.py

--- calcTED.py
+++ calcTED.py
@@ -1,8 +1,12 @@
-import datetime
+#Código para calcular o tempo estimado de download, pede para o usuário inserir o tamanho do arquivo e a velocidade da internet
+
+import datetime #importação de funções de data e hora
 
 tam = float(input("Informe o tamanho do arquivo em Mb: "))
 vel = float(input("Informe o a velocidade do link de internet (Mbps): "))
 
-ted = tam / vel 
+#calculo do tempo em segundos
+ted = tam / vel
 
+#print formatado, a função datetime.timedelta(seconds=~segundos~) converte o tempo em segundos em formtado de horas "00:00:00"
 print("Tempo estimado de download:", datetime.timedelta(seconds=ted))
